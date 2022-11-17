6362e66cc34dce2d5b7d867ad90fe539c43d5cf1
Medox36 <giuntinil@bzz.ch>
2022-11-17 21:40:48 +0100

    added and implemented TurnScreenBlack main class

---
+++ src/main/java/ch/giuntini/turnscreenblack/TurnScreenBlack.java
@@ -0,0 +1,10 @@
+package ch.giuntini.turnscreenblack;
+
+import java.awt.AWTException;
+
+public class TurnScreenBlack {
+
+    public static void main(String[] args) throws AWTException {
+        new TrayIco();
+    }
+}
